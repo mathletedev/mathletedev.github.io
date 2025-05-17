6767270dd1266eb3a891a2957da4dc411a87ada4
Neal Wang <nealwang.sh@protonmail.com>
2025-05-16 22:33:13 -0700

    feat: vertical scrolling

--- src/lib/main.ts
+++ src/lib/main.ts
@@ -3,37 +3,59 @@ import ScrollToPlugin from "gsap/ScrollToPlugin";
 import ScrollTrigger from "gsap/ScrollTrigger";
 
 const init = () => {
-    if (!document.querySelector(".panel-container")) {
+    if (!document.querySelector(".panel")) {
         return;
     }
 
-    gsap.registerPlugin(ScrollTrigger);
     gsap.registerPlugin(ScrollToPlugin);
+    gsap.registerPlugin(ScrollTrigger);
 
-    let panels = gsap.utils.toArray(".panel");
+    const panels = gsap.utils.toArray(".panel");
+    const sts: ScrollTrigger[] = [];
 
-    gsap.to(panels, {
-        xPercent: -100 * (panels.length - 1),
-        ease: "none",
-        scrollTrigger: {
-            trigger: ".panel-container",
+    panels.forEach((panel, i) => {
+        const st = ScrollTrigger.create({
+            trigger: panel as gsap.DOMTarget,
+            start: "top top",
+            end: `+=${(document.querySelector(".panel") as HTMLElement).offsetHeight}`,
             pin: true,
-            scrub: 1,
-            snap: 1 / (panels.length - 1),
-            end: () =>
-                "+=" +
-                (document.querySelector(".panel-container") as HTMLElement)
-                    .offsetWidth,
-        },
+            pinSpacing: false,
+            snap: 1,
+        });
+
+        sts.push(st);
     });
 
     for (const button of document.querySelectorAll(
         "nav button",
     ) as NodeListOf<HTMLButtonElement>) {
-        button.onclick = () => {
-            gsap.to(window, {
+        button.onclick = async () => {
+            const panel = document.getElementById(
+                button.innerHTML.toLowerCase(),
+            ) as HTMLElement | null;
+            if (!panel) {
+                return;
+            }
+
+            const above = panel.getBoundingClientRect().top <= 0;
+
+            if (above) {
+                sts.forEach((st) => {
+                    st.disable();
+                });
+            }
+
+            await gsap.to(window, {
                 scrollTo: `#${button.innerHTML.toLowerCase()}`,
+                duration: 2,
+                ease: "power2.out",
             });
+
+            if (above) {
+                sts.forEach((st) => {
+                    st.enable();
+                });
+            }
         };
     }
 };
