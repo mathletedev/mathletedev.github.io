9696052fb994b2838a0409022385e6714118d923
Neal Wang <nealwang.sh@protonmail.com>
2025-06-20 00:00:57 -0700

    feat: notes

--- src/scenes/Synthwave.svelte
+++ src/scenes/Synthwave.svelte
@@ -13,13 +13,22 @@
     import { EffectComposer, RenderPass } from "three/examples/jsm/Addons.js";
 
     const PEAK_DECAY = 8;
-    const MP_OFFSET = 0.1;
+    // in seconds
+    const MP_OFFSET = 0.2;
+    // in units per second
+    const NOTE_SPEED = 10;
+    // in units
+    const NOTE_SPAWN_DISTANCE = 5;
+    // in seconds
+    const NOTE_SPAWN_OFFSET = NOTE_SPAWN_DISTANCE / NOTE_SPEED;
 
     const { scene, camera, renderer, renderStage } = useThrelte();
 
     let time = $state(0);
     let composer: EffectComposer | null = $state(null);
     let peak = $state(0);
+    let prevNotePeak = $state(0);
+    let notes: THREE.Vector2[] = $state([]);
 
     let gridMaterial = $state(
         new THREE.ShaderMaterial({
@@ -55,6 +64,7 @@
 
         composer = new EffectComposer(renderer);
         composer.addPass(new RenderPass(scene, camera.current));
+        // BUG: bloom makes everything look dull
         /* composer.addPass(
             new UnrealBloomPass(
                 new THREE.Vector2(innerWidth, innerHeight),
@@ -90,6 +100,34 @@
 
         gridMaterial.uniforms.uPeak.value = peak;
         sunMaterial.uniforms.uPeak.value = peak;
+
+        for (let i = 0; i < notes.length; i++) {
+            notes[i] = new THREE.Vector2(
+                notes[i].x,
+                notes[i].y - NOTE_SPEED * delta,
+            );
+        }
+        notes = notes.filter((note) => note.y > 0);
+
+        if (mp.paused) {
+            return;
+        }
+
+        nextPeak = getPeak(
+            mp.audioEl.currentTime + MP_OFFSET + NOTE_SPAWN_OFFSET,
+        );
+        if (nextPeak === -1) {
+            return;
+        }
+
+        if (nextPeak > prevNotePeak) {
+            notes.push(
+                new THREE.Vector2(Math.random() * 2 - 1, NOTE_SPAWN_DISTANCE),
+            );
+            prevNotePeak = nextPeak;
+        } else {
+            prevNotePeak += (nextPeak - prevNotePeak) * PEAK_DECAY * delta;
+        }
     });
 
     useTask(
@@ -102,10 +140,24 @@
     );
 </script>
 
+<T.AmbientLight intensity={0.5} />
+<T.DirectionalLight position={[0, 1, 0]} intensity={1} />
+
 <T.Mesh position.y={0} position.z={-10} material={sunMaterial}>
     <T.PlaneGeometry args={[20 + peak / 100, 20 + peak / 100]} />
 </T.Mesh>
 
-<T.Mesh position.y={-1.1} rotation.x={TAU * -0.25} material={gridMaterial}>
-    <T.PlaneGeometry args={[30, 10]} />
-</T.Mesh>
+<T.Group position.y={-1.1} rotation.x={TAU * -0.25}>
+    {#each notes as note}
+        <T.Mesh position={[note.x, -3 + note.y, 0.1]}>
+            <T.SphereGeometry args={[0.1, 8, 8]} />
+            <T.MeshPhongMaterial
+                emissive="#0000ff"
+                emissiveIntensity={(peak / 100) * 10}
+            />
+        </T.Mesh>
+    {/each}
+    <T.Mesh material={gridMaterial}>
+        <T.PlaneGeometry args={[30, 10]} />
+    </T.Mesh>
+</T.Group>
